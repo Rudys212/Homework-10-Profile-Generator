f386d3036ac351d1d391acca0b4b2ee63a71090b
Rudy Sanchez <rudys212@gmail.com>
2021-06-09 13:53:02 -0700

    set up classes and files

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require("inquirer");
+const fs = require("fs)");
+
+const engineer = require("./lib/Engineer");
+const intern = require("./lib/Intern");
+const manager = require("./lib/Manager");
+const employee = require("./lib/Employee");
+
+const employees = [];
---
+++ test/Manager.test.js
@@ -0,0 +1,19 @@
+// *name`
+
+// * `id`
+
+// * `email`
+
+// * `getName()`
+
+// * `getId()`
+
+// * `getEmail()`
+
+// * `getRole()`&mdash;returns `'Employee'`
+
+// In addition to `Employee`'s properties and methods, `Manager` will also have the following:
+
+// * `officeNumber`
+
+// * `getRole()`&mdash;overridden to return `'Manager'`
